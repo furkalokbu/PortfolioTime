dc062e95853f96c47f6d460360667bec1c2e1832
Serhii <furkalokbu@gmail.com>
2021-05-14 18:27:25 +0300

    bag

--- projects/views.py
+++ projects/views.py
@@ -6,18 +6,18 @@ from .models import Portfolio
 
 def Home(request):
     template_name = "index.html"
-    # portfolios = Portfolio.objects.all()
-    # context = {'portfolios': portfolios}
-    response = requests.get("http://127.0.0.1:8000/api/portfolio/")
+    portfolios = Portfolio.objects.all()
+    context = {'portfolios': portfolios}
+    # response = requests.get("http://127.0.0.1:8000/api/portfolio/")
 
-    if response.status_code == 200:
-        context = {
-            "portfolios": response.json(),
-        }
-    else:
-        context = {
-            "portfolios": {},
-            "error": "Bad response!"}
+    # if response.status_code == 200:
+    #     context = {
+    #         "portfolios": response.json(),
+    #     }
+    # else:
+    #     context = {
+    #         "portfolios": {},
+    #         "error": "Bad response!"}
 
 
     return render(request, template_name, context)
@@ -25,9 +25,7 @@ def Home(request):
 def PortfolioDetail(request, pk):
     
     template_name = "detail.html"
-    
     detail = Portfolio.objects.filter(pk=pk).first()
-
     context = {
         'detail': detail
     }
